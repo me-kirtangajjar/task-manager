fd99fbfdbe1aa89559de06116d2bc5970f268973
Kirtan Gajjar <mekirtangajjar@gmail.com>
2024-07-12 18:57:58 +0530

    completed get endpoints

--- app.js
+++ app.js
@@ -1,15 +1,39 @@
 const express = require("express");
 const app = express();
-const port = 3000;
+const allTasks = require("./task.json").tasks;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.listen(port, (err) => {
+// Retrieve all tasks
+app.get("/tasks", (req, res) => {
+  return res
+    .status(200)
+    .send({ msg: "All task details fetched successfully", data: allTasks });
+});
+
+// Retrieve a single task by its ID.
+app.get("/tasks/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+
+  if (isNaN(id)) {
+    return res.status(400).send({ msg: "Invalid task ID" });
+  }
+
+  const taskById = allTasks.find((task) => task.id === id);
+
+  if (!taskById) return res.status(404).send({ msg: "Task not found" });
+
+  return res
+    .status(200)
+    .send({ msg: "Task details fetched successfully", data: taskById });
+});
+
+app.listen(process.env.PORT || 3000, (err) => {
   if (err) {
     return console.log("Something bad happened", err);
   }
-  console.log(`Server is listening on ${port}`);
+  console.log(`Server is listening on ${process.env.PORT || 3000}`);
 });
 
 module.exports = app;
