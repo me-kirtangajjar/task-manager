b16cfe66f09c336f73e2ff67bf7b8822e5068c34
Kirtan Gajjar <mekirtangajjar@gmail.com>
2024-07-14 18:31:23 +0530

    Added endpoints for put and delete

--- app.js
+++ app.js
@@ -59,6 +59,64 @@ app.post("/tasks", (req, res) => {
     .send({ msg: "Task created successfully", data: newTask });
 });
 
+app.put("/tasks/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  const { title, description, completed } = req.body;
+
+  if (isNaN(id)) {
+    return res.status(400).send({ msg: "Invalid request" });
+  }
+
+  if (Object.keys(req.body).length !== 3) {
+    return res.status(400).send({
+      msg: "Invalid request body: properties required - title, description, completed",
+    });
+  }
+
+  if (
+    title.length === 0 ||
+    description.length === 0 ||
+    typeof completed !== "boolean"
+  ) {
+    return res.status(400).send({ msg: "Invalid request body" });
+  }
+
+  const taskById = allTasks.find((task) => task.id === id);
+  if (!taskById) {
+    return res.status(404).send({ msg: "Task not found" });
+  }
+
+  const index = allTasks.indexOf(taskById);
+
+  allTasks[index].title = title;
+  allTasks[index].description = description;
+  allTasks[index].completed = completed;
+
+  return res
+    .status(200)
+    .send({ msg: "Task details updated successfully", data: allTasks[index] });
+});
+
+app.delete("/tasks/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+
+  if (isNaN(id)) {
+    return res.status(400).send({ msg: "Invalid request" });
+  }
+
+  const taskById = allTasks.find((task) => task.id === id);
+  if (!taskById) {
+    return res.status(404).send({ msg: "Task not found" });
+  }
+
+  const index = allTasks.indexOf(taskById);
+  allTasks.splice(index, 1);
+
+  return res
+    .status(200)
+    .send({ msg: "Task deleted successfully", data: taskById });
+});
+
 app.listen(process.env.PORT || 3000, (err) => {
   if (err) {
     return console.log("Something bad happened", err);
